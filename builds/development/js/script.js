ebd6b7487988efdf4eb8e83ac924419a65936279
mfaibrahem <mfaibrahem2013@gmail.com>
2016-04-16 22:41:26 +0200

    mfa update

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -1,7 +1,7 @@
 
 
  //-------------------------------
-  var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute']);
 
     // configure our routes
     app.config(function($routeProvider) {
@@ -12,13 +12,12 @@
                 templateUrl : 'pages/home.html',
                 controller  : 'PostController'
             })
+            .when('/register', {
+                templateUrl : 'pages/Signup.html',
+                controller  : 'signupController'
+            })
             
-//            .when('/register', {
-//              templateUrl : 'pages/Signup.html',
-//              controller  : 'signupController'
-//          })
-
-
+            
             // route for the about page
             .when('/search', {
                 templateUrl : 'pages/search.html',
@@ -28,165 +27,281 @@
             // route for the about page
             .when('/explore', {
                 templateUrl : 'pages/explore.html',
-                controller  : 'exploreController'
+                
+                
+
             })
+            .when('/language/:lanid', {
+                templateUrl: 'pages/langsnippets.html',
+                controller: 'expController'
+
+
+      })
+        
             // route for the about page
             .when('/add', {
                 templateUrl : 'pages/add.html',
                 controller  : 'addController'
             })
-
+            
             // route for the contact page
             .when('/request', {
                 templateUrl : 'pages/request.html',
                 controller  : 'requestController'
             })
 
-             .otherwise({redirectTo: 'pages/Signin.html'});
+             .otherwise({redirectTo: 'pages/Signin.html'
+         });
     });
+
+/*run.$inject = ['$rootScope', '$location', '$cookieStore', '$http'];
+    function run($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+        }
+ 
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in and trying to access a restricted page
+            var restrictedPage = $.inArray($location.path(), ['/Signin', '/register']) === -1;
+            var loggedIn = $rootScope.globals.currentUser;
+            if (restrictedPage && !loggedIn) {
+                $location.path('/Signin');
+            }
+            */
 
 
  //-------------------------------
-app.controller('PostController', ['$scope', '$http', function($scope, $http) {
+app.controller('PostController', ['$scope', '$http','$rootScope', function($scope, $http,$rootScope) {
+		
+		this.postForm = function() {
+      
+		var obj = {'username': $scope.inputData.username,'password': $scope.inputData.password};
+			console.log(JSON.stringify(obj));
 
-this.postForm = function() {
-var obj = {'username': $scope.inputData.username,'password': $scope.inputData.password};
-	console.log(JSON.stringify(obj));
-
-	$http({
-
-		method: 'POST',
-		url: 'http://172.17.42.1:6543/login',
-		data:JSON.stringify(obj),
-		headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-	})
-	.success(function(data, status, headers, config) {
-		//	window.location.href = 'pages/home.html';
-			console.log(data);
-			console.log(status);
-				})
-	.error(function(data, status, headers, config) {
-	//	$scope.errorMsg = 'Unable to submit form';
-		console.log(data);
-		console.log(status);
-
-
-	})
-}
-
-}]);
-
-app.controller('signupController', ['$scope', '$http', function($scope, $http) {
-
-this.signupForm = function() {
-var upobject = {'first_name':$scope.signupData.firstname,
-'last_name': $scope.signupData.lastname,
-'username': $scope.signupData.username,
-'email': $scope.signupData.email,
-'password': $scope.signupData.password,
-'avatar': $scope.signupData.avatar,
-'country': $scope.signupData.country
-			 };
-						console.log(JSON.stringify(upobject));
-
-
-	$http({
-		method: 'POST',
-		url: 'http://172.17.42.1:6543/signup',
-		data:JSON.stringify(upobject),
-		headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-	})
-	.success(function(data, status, headers, config) {
-			//window.location.href = 'index.html';
-			console.log(data);
-			console.log(status);
-				})
-	.error(function(data, status, headers, config) {
-		//$scope.errorMsg = 'Unable to signup the form';
-		console.log(data);
-		console.log(status);
-
-
-	})
-}
-
-}]);
-
-app.controller('addController', ['$scope', '$http', function($scope, $http) {
-	$http.get("http://172.17.42.1:6543/elements")
-		 .success(function(data) {
-			$scope.result=data;
-			//$scope.languages = result['language']['name'];
-			//$scope.contexts = result['context']['name'];
-			//$scope.types =result['code_type']['name'];
-			console.log(data);
-	console.log(status);
-	$scope.lan = [];
-	angular.forEach($scope.result.language, function(item) {
-				 $scope.lan.push(item);
-				})
-	$scope.con = [];
-	angular.forEach($scope.result.context, function(item) {
-				 $scope.con.push(item);
-				})
-	$scope.typ = [];
-	angular.forEach($scope.result.code_type, function(item) {
-				$scope.typ.push(item);
-				})
-
-			 })
-		}]);
-
-
-app.controller('snippetController', ['$scope', '$http', function($scope, $http) {
-
-		$scope.Postsnippet=function(){
-		var snap = {'title': $scope.inputData.Title,
-								'description': $scope.inputData.Description,
-								'code': $scope.inputData.Code,
-								'context': $scope.inputData.Context.id,
-								'tags': $scope.inputData.Tags,
-								'language': $scope.inputData.Language.id,
-								'code_type': $scope.inputData.Codetype.id,
-						};
 			$http({
-		method: 'POST',
-		url: 'http://172.17.42.1:6543/create/snippet',
-		data:JSON.stringify(snap),
-		headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-			 })
-				.success(function(data, status, headers, config) {
-			console.log(data);
-			console.log(status);
-								})
+
+				method: 'POST',
+				url: 'http://www.koodet.com:6543/api/login',
+				data:JSON.stringify(obj),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			})
+			.success(function(data, status, headers, config) {
+					console.log(data);
+					console.log(status);
+          $rootScope.currentUserSignedIn = true;
+      //  $rootScope.currentUser.username = data.username;
+            })
+			.error(function(data, status, headers, config) {
+				//$scope.errorMsg = 'Unable to submit form';
+				console.log(data);
+				console.log(status);
+
+
+			})
 		}
-		}]);
+		
+	}]);
+
+	app.controller('signupController', ['$scope', '$http', function($scope, $http) {
+		
+		this.signupForm = function() {
+		var upobject = {'first_name':$scope.signupData.firstname,
+		'last_name': $scope.signupData.lastname,
+		'username': $scope.signupData.username,
+		'email': $scope.signupData.email,
+		'password': $scope.signupData.password,
+		'avatar': $scope.signupData.avatar,
+		'country': $scope.signupData.country
+           };
+           			console.log(JSON.stringify(upobject));
 
 
-//app.controller('starController', function($scope) {
+			$http({
+				method: 'POST',
+				url: 'http://www.koodet.com:6543/api/users',
+				data:JSON.stringify(upobject),
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			})
+			.success(function(data, status, headers, config) {
+					//window.location.href = 'index.html';
+					console.log(data);
+					console.log(status);
+            })
+			.error(function(data, status, headers, config) {
+				//$scope.errorMsg = 'Unable to signup the form';
+				console.log(data);
+				console.log(status);
+			})
+		}
+		
+	}]);	
 
-	//$scope.rating = 5;
+	app.controller('addController', ['$scope', '$http', function($scope, $http) {
+    	$http.get("http://www.koodet.com:6543/api/elements")
+         .success(function(data) {
+         	$scope.result=data;
+         	console.log(data);
+			console.log(status);
+			$scope.lan = [];
+			angular.forEach($scope.result.language, function(item) {
+             $scope.lan.push(item);
+            })
+			$scope.con = [];
+			angular.forEach($scope.result.context, function(item) {
+             $scope.con.push(item);
+            })
+			$scope.typ = [];
+			angular.forEach($scope.result.code_type, function(item) {
+            $scope.typ.push(item);
+            })
+         
+           })
 
-//});
+         $scope.Postsnippet=function(){
+        var snap = {'title': $scope.inputData.Title,
+                    'description': $scope.inputData.Description,
+                    'code': $scope.inputData.Code,
+                    'context': $scope.inputData.Context.id,
+                    'tags': $scope.inputData.Tags,
+                    'language': $scope.inputData.Language.id,
+                    'code_type': $scope.inputData.Codetype.id,
+                };
+          $http({
+        method: 'POST',
+        url: 'http://www.koodet.com:6543/api/snippets',
+        data:JSON.stringify(snap),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+           })
+            .success(function(data, status, headers, config) {
+          console.log(data);
+          console.log(status);
+
+                    })
+        }
+        
+
+
+        }]);
 
 
 
 
+app.controller('starCtrl', function($scope,$http) {
+    $scope.rating = 0;
+    $scope.rateFunction = function(rating) {
+      var obj = {rating: rating};
+ 
+         $http({
+
+        method: 'POST',
+        url: 'http://www.koodet.com:6543/api',
+        data:JSON.stringify(obj),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      })
+      .success(function(data, status, headers, config) {
+          console.log(data);
+          console.log(status);
+            })
+      .error(function(data, status, headers, config) {
+        console.log(data);
+        console.log(status);
 
 
-app.controller('searchController', function($scope) {
-		$scope.message = 'searchController page';
-});
-
-app.controller('exploreController', function($scope) {
-		$scope.message = 'LexploreController page';
-});
+      })
+    };
 
 
+  });
 
-app.controller('requestController', function($scope) {
-		$scope.message = 'requestController page';
-});
+app.directive('starRating', function () {
+    return {
+      restrict: 'A',
+      template: '<ul class="rating">' +
+                  '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '=',
+        max: '=',
+        readonly: '@',
+        onRatingSelected: '&'
+      },
+      link: function (scope, elem, attrs) {
+
+        var updateStars = function() {
+          scope.stars = [];
+          for (var  i = 0; i < scope.max; i++) {
+            scope.stars.push({filled: i < scope.ratingValue});
+          }
+        };
+
+        scope.toggle = function(index) {
+          if (scope.readonly && scope.readonly === 'true') {
+            return;
+          }
+          scope.ratingValue = index +1;
+          scope.onRatingSelected({rating: index +1});
+        };
+
+        scope.$watch('ratingValue', function(newVal, oldVal) {
+          if (newVal || newVal === 0) {
+            updateStars();
+          }
+        });
+      }
+    }
+  });
+
+
+/*app.controller('expController', ['$scope', '$http','$routeParams', function($scope, $http,$routeParams) {
+      var parameters = $routeParams.langId;
+      var config = {
+        params: parameters
+      };
+
+        $http.get("http://41.35.100.129:6543/api/explore/snippets/langId")
+         .success(function(data) {
+          $scope.result=data;
+          console.log(data);
+      console.log(status);
+      $scope.tit = [];
+      angular.forEach($scope.result.title, function(item) {
+             $scope.tit.push(item);
+            })
+        })
+      }]);
+*/
+
+app.controller('expController', ['$scope', '$http','$routeParams', function($scope, $http,$routeParams) {
+  var snap = {
+                    'language':$routeParams.lanid
+            };
+
+        $http({
+        method: 'POST',
+
+        url: 'http://41.35.118.106:6543/api/explore/snippets',
+        data:JSON.stringify(snap),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+           })
+            .success(function(data, status, headers, config) {
+          console.log(data);
+          console.log(status);
+          $scope.result=data;
+
+          $scope.tit = [];
+          angular.forEach($scope.result, function(item) {
+             $scope.tit.push(item);
+             console.log(item.title);
+            }
+            )
+          
+
+                    })
+                }]);
 
 
  //-------------------------------
