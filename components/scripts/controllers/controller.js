0ba40ebefe024cbcf3542be12f24e3418500c48a
Mahmoud Magdy <engmahmoud2016@gmail.com>
2016-04-19 20:24:28 +0200

    Update controller.js

--- components/scripts/controllers/controller.js
+++ components/scripts/controllers/controller.js
@@ -181,24 +181,54 @@ app.directive('starRating', function () {
   });
 
 
-/*app.controller('expController', ['$scope', '$http','$routeParams', function($scope, $http,$routeParams) {
-      var parameters = $routeParams.langId;
-      var config = {
-        params: parameters
-      };
-
-        $http.get("http://41.35.100.129:6543/api/explore/snippets/langId")
+app.controller('requestController', ['$scope', '$http', function($scope, $http) {
+      $http.get("http://www.koodet.com:6543/api/elements")
          .success(function(data) {
           $scope.result=data;
           console.log(data);
       console.log(status);
-      $scope.tit = [];
-      angular.forEach($scope.result.title, function(item) {
-             $scope.tit.push(item);
+      $scope.lan = [];
+      angular.forEach($scope.result.language, function(item) {
+             $scope.lan.push(item);
             })
-        })
-      }]);
-*/
+      $scope.con = [];
+      angular.forEach($scope.result.context, function(item) {
+             $scope.con.push(item);
+            })
+      $scope.typ = [];
+      angular.forEach($scope.result.code_type, function(item) {
+            $scope.typ.push(item);
+            })
+         
+           })
+
+         $scope.Postquestion=function(){
+        var snap = {'title': $scope.inputData.Title,
+                    'description': $scope.inputData.Description,
+                    'context': $scope.inputData.Context.id,
+                    'tags': $scope.inputData.Tags,
+                    'language': $scope.inputData.Language.id,
+                    'code_type': $scope.inputData.Codetype.id,
+                };
+          $http({
+        method: 'POST',
+        url: 'http://www.koodet.com:6543/api/questions',
+        data:JSON.stringify(snap),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+           })
+            .success(function(data, status, headers, config) {
+          console.log(data);
+          console.log(status);
+
+                    })
+        }
+        
+
+
+        }]);
+
+
+
 
 app.controller('expController', ['$scope', '$http','$routeParams', function($scope, $http,$routeParams) {
   var snap = {
