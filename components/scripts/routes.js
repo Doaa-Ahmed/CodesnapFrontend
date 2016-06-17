81788bb67731dcc5d8ebcdb69cd43d1a2b41c593
Amr Ragaey <amragaey@gmail.com>
2016-06-17 14:12:54 +0200

    undo hash remove

--- components/scripts/routes.js
+++ components/scripts/routes.js
@@ -62,17 +62,17 @@ function configurator($routeProvider, $httpProvider, $locationProvider) {
             //withCredentials: true
         });
 
-    if (window.history && window.history.pushState) {
-        //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+    // if (window.history && window.history.pushState) {
+    //     //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
 
-        // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+    //     // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
 
-        // if you don't wish to set base URL then use this
-        $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-    }
+    //     // if you don't wish to set base URL then use this
+    //     $locationProvider.html5Mode({
+    //         enabled: true,
+    //         requireBase: false
+    //     });
+    // }
 }
 /*app.run(['$rootScope', '$location', 'Auth', function ($rootScope, $location, Auth) {
     $rootScope.$on('$routeChangeStart', function (event) {
