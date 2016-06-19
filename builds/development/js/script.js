32ea0acfbe752127f5340fa8e32d094e492affcd
ahmedmedhat <medhat1471993@gmail.com>
2016-06-19 08:11:14 +0200

    enable html5 , remove # from urls

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -631,6 +631,8 @@ function configurator($routeProvider, $httpProvider, $locationProvider) {
             //withCredentials: true
         });
 
+        $locationProvider.html5Mode(true);
+
     // if (window.history && window.history.pushState) {
     //     //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
 
