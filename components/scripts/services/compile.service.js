ec6f99518c42eee838c6b592d59f3807b07b0717
Amr Ragaey <amragaey@gmail.com>
2016-07-05 19:26:01 +0200

    editing compile to server

--- components/scripts/services/compile.service.js
+++ components/scripts/services/compile.service.js
@@ -12,17 +12,17 @@ function compileService($http) {
     return service;
 
     function getLanguages() {
-        return $http.get('http://localhost:6543/api/languages')
+        return $http.get('http://www.koodet.com:6543/api/languages')
                     .then(handleSuccess, handleError('Error getting Languages'));
     }
 
     function getLangSample(languageID) {
-        return $http.get('http://localhost:6543/api/compile/' + languageID)
+        return $http.get('http://www.koodet.com:6543/api/compile/' + languageID)
                     .then(handleSuccess, handleError('Error getting Languages'));
     }
 
     function compileSnippet(language, snippet, stdin) {
-        return $http.post('http://localhost:6543/api/compile', {"language":language, "snippet":snippet, "stdin":stdin})
+        return $http.post('http://www.koodet.com:6543/api/compile', {"language":language, "snippet":snippet, "stdin":stdin})
                     .then(handleSuccess, handleError('Error creating presentation'));
     }
 
